60a3d35c7f9cf0435ac58cc2c959ff7eb36220ac
Jam0048 <amigj@berea.edu>
2024-12-08 17:19:59 -0500

    Start screen seems done. Working on game code and end screen code. Added more links/updated plan

--- original_code/game.py
+++ original_code/game.py
@@ -30,9 +30,17 @@ class Game:
         """
 
         """
+        self.wn_size = 800,600
+        self.wn = pygame.display.set_mode(self.wn_size)
+        self.running = True
+        self.wn.fill("white")
+        self.time = pygame.time.Clock()
+        #maybe objects for characters like T11
+            #self.scott = character-class/ig?(self.wn_size)
+            #self.wizzle
+            #self.wilborne
 
-        #creates the window
-        # game clock/tick
+
         # game logic creation
 
 
@@ -42,6 +50,12 @@ class Game:
         :return:
         """
 
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+
         #runs the game. first opens with start screen call
         #after start screen then story construction
 
@@ -49,17 +63,24 @@ class Game:
 
 
         #story
-            #late at night and wake up for cookies, but oh no its gone
+        #late at night and wake up for cookies, but oh no its gone
 
-            #option to look around: the fridge, pocket, shadow realm etc...
-                #pocket: the pocket has lint and bugs in it but no cookies.
-                #fridge: a note reading sorry I ate your cookies
-                #darkweb: the basement did not have your cookies. Besides I think its a bit too dangerous looking in there for them
+        #option to look around: the fridge, pocket, shadow realm etc...
+        #pocket: the pocket has lint and bugs in it but no cookies.
+        #fridge: a note reading sorry I ate your cookies
+        #darkweb: the basement did not have your cookies. Besides I think its a bit too dangerous looking in there for them
 
-            #you ask/blame roomie but roomie is clueless
-            #You found cookies! but its internet cookies...Ah great...Lose
-            #you find the wizzle the weasel wizard and ask for more cookies
-            #the wizzle is dissappointed but gives you it's cookies. Fun fact the chips are not chocolate. Win
+        #you ask/blame roomie but roomie is clueless
+        #You found cookies! but its internet cookies...Ah great...Lose
+        #you find the wizzle the weasel wizard and ask for more cookies
+        #the wizzle is dissappointed but gives you it's cookies. Fun fact the chips are not chocolate. Win
+
+
+
+
+
+        pygame.QUIT
+
 
 
 def main():
@@ -67,10 +88,10 @@ def main():
     """
 
     """
-    start_game = start_screen.main()
-
+    # start_game = start_screen.main()
+    Game()
     #game = Game()
-    #game.run()
+    #game.__init__()
 
 
 if __name__ == "__main__":
